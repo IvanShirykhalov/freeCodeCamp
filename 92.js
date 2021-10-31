798e45a332761b26fcd6617988ffced777ea1641
IvanShirykhalov <80598280+IvanShirykhalov@users.noreply.github.com>
2021-10-31 18:26:44 +0300

    Add files via upload

---
+++ 92.js
@@ -0,0 +1,15 @@
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Eminem",
+    title: "Slimshady",
+    release_year: 2001,
+    formats: ["CD", "DVD"],
+  },
+];
