63455820099a24bd901aaa041df4f08d90e68139
IvanShirykhalov <80598280+IvanShirykhalov@users.noreply.github.com>
2021-11-09 20:59:42 +0500

    Add files via upload

---
+++ Basic Data Structures/7.js
@@ -0,0 +1,10 @@
+function forecast(arr) {
+  // Only change code below this line
+  let myArr = arr.slice(2, 4);
+  return myArr;
+}
+
+// Only change code above this line
+console.log(
+  forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
+);
